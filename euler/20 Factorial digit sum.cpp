22b38477461b62a5d604fa7103fc539842489196
Quinn Perfetto <thequinnftw@gmail.com>
2014-10-24 17:59:31 -0400

    calculated 100! using my bigint library

---
+++ euler/20 Factorial digit sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "BigInteger.hpp"
+
+int main(void){
+	BigInteger x("100");
+	for(int i = 99; i >= 2; i--)
+		x *= i;
+	int ans = 0;
+	for(char c:x)
+		ans += c-'0';
+	std::cout << ans << std::endl;
+	return 0;
+}
