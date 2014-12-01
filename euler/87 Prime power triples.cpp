89c47235b71f0ad6276253e0cf8815fbc47bcb25
Quinn Perfetto <thequinnftw@gmail.com>
2014-12-01 10:47:11 -0500

    solved 87

--- euler/87 Prime power triples.cpp
+++ euler/87 Prime power triples.cpp
@@ -3,17 +3,17 @@
 #include <set>
 #include "PrimeSieve.hpp"
 
-int pow_2(int);
-int pow_3(int);
-int pow_4(int);
+long pow_2(long);
+long pow_3(long);
+long pow_4(long);
 
 int main(){
 	int cap = 50000000;
-	PrimeSieve p(7071);
+	PrimeSieve p(7072);
 	std::vector<int> primes = p.to_vector();
-	std::set<int> ans;
+	std::set<long> ans;
 	for(auto i:primes){
-		for(auto j:primes){
+ 		for(auto j:primes){
 			for(auto k:primes){
 				long tmp = pow_2(i) + pow_3(j) + pow_4(k);
 				if(tmp > cap) break;
@@ -25,12 +25,12 @@ int main(){
 	return 0;
 }
 
-int pow_2(int x){
+long pow_2(long x){
 	return x * x;
 }
-int pow_3(int x){
+long pow_3(long x){
 	return x * pow_2(x);
 }
-int pow_4(int x){
+long pow_4(long x){
 	return x * pow_3(x);
 }
