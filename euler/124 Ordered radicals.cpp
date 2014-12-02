58847c464097e0a35e3e921f90bc882acbccd986
Quinn Perfetto <thequinnftw@gmail.com>
2014-12-01 20:10:53 -0500

    solved 124

---
+++ euler/124 Ordered radicals.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "PrimeSieve.hpp"
+
+bool compare(std::pair<int, int> x, std::pair<int, int> y) {
+	if(x.second == y.second)
+		return x.first < y.first;
+	return x.second < y.second;
+}
+
+int rad(int, std::vector<int> const&);
+
+int main(){
+	PrimeSieve p(100001);
+	std::vector<int> primes = p.to_vector();
+	std::vector <std::pair<int, int>> v;
+	for(int i = 1; i <= 100000; i++)
+		v.push_back(std::make_pair(i, rad(i, primes)));
+	std::sort(v.begin(), v.end(), compare);
+	std::cout << v[9999].first << std::endl;
+	return 0;
+}
+
+int rad(int x, std::vector<int> const& primes){
+	int ans = 1;
+	int i = 0;
+	while(x > 1){
+		if(x % primes[i] == 0)
+			ans *= primes[i];
+		while(x % primes[i] == 0)
+			x /= primes[i];
+		i++;
+	}
+	return ans;
+}
