b4926bea682b19f535d44f8c531a9b6923b9c8c3
Quinn Perfetto <thequinnftw@gmail.com>
2014-12-29 10:36:54 -0500

    optimized 179 drastically by sieving multiples

--- euler/179 Consecutive positive divisors.cpp
+++ euler/179 Consecutive positive divisors.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
-#define LIMIT 10000000
-
-int divisors(int);
 
 int main(){
-	int ans = 0;
-	int last = divisors(2);
-	for(int i = 3; i < LIMIT; i++){
-		int cur = divisors(i);
-		if(cur == last) ans++;
-		last = cur;
-	}
-	std::cout << ans << std::endl;
-	return 0;
-}
-
-int divisors(int n){
-	int ans = 2;
-	for(int i = 2; i * i <= n; i++){
-		if(n % i == 0){
-			ans++;
-			if(i * i != n) ans++;
+	const long LIMIT = 10000000;
+	int* divisors = new int[LIMIT + 1];
+	memset(divisors, 0, LIMIT + 1);
+	for(int i = 2; i <= LIMIT / 2; i++){
+		for(int j = 2; i * j < LIMIT; j++){
+			divisors[i * j]++;
 		}
 	}
-	return ans;
+	
+	long ans = 0;
+	for(int i = 2; i < LIMIT; i++){
+		if(divisors[i] == divisors[i + 1]) ans++;
+	}
+	std::cout << ans << std::endl;
+	delete[] divisors;
+	return 0;
 }
