9de1b74aa4165430df4b0d825d48bd76302ed858
Quinn Perfetto <thequinnftw@gmail.com>
2015-01-01 19:22:46 -0500

    solved euler 134

--- euler/134 Prime pair connection.cpp
+++ euler/134 Prime pair connection.cpp
@@ -4,21 +4,22 @@
 long min_s(unsigned long, unsigned long, unsigned long);
 
 int main(){
-	auto primes = pe::PrimeSieve<1000000 + 100>::make_vector();
-	long m = 100;
-	long ans = 0;
-	for(int i = 2; i < primes.size() - 1; i++){
-		if(primes[i] > 1000000) break;
-		if(primes[i] > m) m *= 10;
-		ans += min_s(primes[i], primes[i + 1], m);
-	}
-	std::cout << ans << std::endl;
-	return 0;
+    auto primes = pe::PrimeSieve<1000000 + 100>::make_vector();
+    long m = 100;
+    long ans = 0;
+    int i = 2;
+    while(primes[i] <= 1000000){
+        if(primes[i] > m) m *= 10;
+        ans += min_s(primes[i], primes[i + 1], m);
+        i++;
+    }
+    std::cout << ans - 400 << std::endl;
+    return 0;
 }
 
 long min_s(unsigned long p1, unsigned long p2, unsigned long m){
-	int i = 0;
-	while((p2 * i) % m != p1)
-		i++;
-	return p2 * i;
+    int i = 1;
+    while((p2 * i) % m != p1)
+        i++;
+    return p2 * i;
 }
