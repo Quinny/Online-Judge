05d92282b56dcb06e07c87befadccee3beceb63a
Quinn Perfetto <thequinnftw@gmail.com>
2015-04-26 15:21:19 -0400

    sped up 95

--- euler/095 Amicable chains.cpp
+++ euler/095 Amicable chains.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <set>
+#include <unordered_map>
 
-int sum_divisors(int);
+//int sum_divisors(int);
 std::pair<int, int> smallest_elem(int);
+
+struct sum_divisors {
+    mutable std::unordered_map<int, int> m_;
+    int operator () (int n) {
+        if (m_[n])
+            return m_[n];
+        int ans = 1;
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                ans += i;
+                if(i * i != n)
+                    ans += n / i;
+            }
+        }
+        return m_[n] = ans;
+    }
+};
+
+sum_divisors sum_div;
 
 int main(){
 	int max_size = 0;
@@ -18,26 +38,15 @@ int main(){
 	return 0;
 }
 
-int sum_divisors(int n){
-	int ans = 1;
-	for(int i = 2; i * i <= n; i++){
-		if(n % i == 0){
-			ans += i;
-			if(i * i != n) ans += n/i;
-		}
-	}
-	return ans;
-}
-
 // chain size, smallest element
-std::pair<int, int> smallest_elem(int start){
+std::pair<int, int> smallest_elem(int start) {
 	std::set<int> chain;
 	chain.insert(start);
-	int m = sum_divisors(start);
-	while(chain.find(m) == chain.end()){
-		if(m > 1000000) return std::make_pair(-1, -1);
-		chain.insert(m);
-		m = sum_divisors(m);
+	int m = sum_div(start);
+	while(chain.insert(m).second) {
+		if(m > 1000000)
+            return std::make_pair(-1, -1);
+		m = sum_div(m);
 	}
 	if(m == start)
 		return std::make_pair(chain.size(), *(chain.begin()));
