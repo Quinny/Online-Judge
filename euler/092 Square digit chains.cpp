f0572129692ac4ed128ed1055008407fe961175b
Quinn Perfetto <thequinnftw@gmail.com>
2015-04-26 15:10:32 -0400

    cleaned up 92

--- euler/092 Square digit chains.cpp
+++ euler/092 Square digit chains.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
-#include <vector>
-#include <unordered_map>
+#include <unordered_set>
 
 #define LIMIT 10000000
 
-int sumSquareDigits(int);
+int sum_square_digits(int);
+bool converges_to_89(int);
 
 int main(void){
-	std::unordered_map<int, int> cache;
-	for(int i = 1; i <= 648; i++){ //the highest possible value for the sum of the digits squared for values < 10 million is 648
-		int tmp = i;
-		while(tmp!=1 && tmp!=89)
-			tmp  = sumSquareDigits(tmp);
-		cache[i] = tmp;
-	}
+    std::unordered_set<int> s;
+	// the highest possible value for the sum of the digits squared for
+    // values < 10 million is 648
 	int ans  = 0;
-	for(int i = 1; i <= LIMIT; i++){
-		if(cache[sumSquareDigits(i)] == 89) ans++;
+    for(int i = 1; i <= 648; i++){
+		if (converges_to_89(i)) {
+            ++ans;
+            s.insert(i);
+        }
+	}
+	for(int i = 649; i <= LIMIT; i++){
+		if (s.find(sum_square_digits(i)) != s.end())
+            ++ans;
 	}
 	std::cout << ans << std::endl;
 
 	return 0;
 }
 
-int sumSquareDigits(int n){
+bool converges_to_89(int i) {
+    while (i != 1 && i != 89)
+        i = sum_square_digits(i);
+    return i == 89;
+}
+
+int sum_square_digits(int n) {
 	int ans = 0;
 	while(n > 0){
-		ans+= (n%10) * (n%10);
-		n/=10;
+		ans += (n % 10) * (n % 10);
+		n /= 10;
 	}
 	return ans;
 }
