4b500c2603166d372930b7462a14cf61082cd62e
Quinn Perfetto <thequinnftw@gmail.com>
2015-04-26 16:10:52 -0400

    cleaned up 98

--- euler/098 Anagramic squares.cpp
+++ euler/098 Anagramic squares.cpp
@@ -5,51 +5,65 @@
 #include <set>
 #include <vector>
 
-int largest_square(std::string, std::string, std::set<int> const&);
+int largest_square(std::string const&, std::string const&, std::set<int> const&);
+bool repeat_mapping(std::map<char, int> const&);
 
 int main(){
-	std::set<int> squares;
-	for(int i = 0; i < 40000; i++) // longest anagram is 8 chars
-		squares.insert(i * i);
-	std::unordered_map<std::string, std::vector<std::string>> anagrams;
-	std::string s, tmp;
-	while(std::cin >> s){
-		tmp = s;
-		std::sort(tmp.begin(), tmp.end());
-		anagrams[tmp].push_back(s); // find the anagrams
-	}
-	int largest = 0;
-	for(auto x : anagrams){
-		if(x.second.size() > 1)
- 			largest = std::max(largest, largest_square(x.second[0], x.second[1], squares));
-	}
-	std::cout << largest << std::endl;
-	return 0;
+    std::set<int> squares;
+    // longest anagram is 8 chars
+    for(int i = 0; i < 40000; i++)
+        squares.insert(i * i);
+    std::unordered_map<std::string, std::vector<std::string>> anagrams;
+    std::string s, tmp;
+    while(std::cin >> s){
+        tmp = s;
+        std::sort(tmp.begin(), tmp.end());
+        // find the anagrams
+        anagrams[tmp].push_back(s);
+    }
+    int largest = 0;
+    for (auto x : anagrams) {
+        if(x.second.size() > 1)
+            largest = std::max(largest,
+                      largest_square(x.second[0], x.second[1], squares));
+    }
+    std::cout << largest << std::endl;
+    return 0;
 }
 
-int largest_square(std::string x, std::string y, std::set<int> const& squares){
-	int largest = 0;
-	for(auto n : squares){
-		std::string tmp = std::to_string(n);
-		if(x.size() != tmp.size()) continue; // check if lenghes differ
-		std::map<char, int> dig;
-		for(int i = x.size() - 1; i >= 0; i--){
-			dig[x[i]] = n % 10; // map characters to integers
-			n /= 10;
-		}
-		std::set<int> s; // check if 2 characters are mapped to the same int
-		bool match = true;
-		for(auto i : dig){
-			if(s.find(i.second) != s.end())
-				match = false;
-			s.insert(i.second);
-		}
-		if(!match) continue; // if we found a repeat
-		if(dig[y[0]] == 0) continue; // leading 0's are not permitted dude
-		int check = 0;
-		for(int i = 0; i < y.size(); i++)
-			check = (10 * check) + (dig[y[i]]);
-		if(squares.find(check) != squares.end() && check > largest) largest = check;
-	}
-	return largest;
+bool repeat_mapping(std::map<char, int> const& m) {
+    bool o[10] = {false};
+    for(auto i : m) {
+        if (o[i.second])
+            return true;
+        o[i.second] = true;
+    }
+    return false;
+}
+
+int largest_square
+(std::string const& x, std::string const& y, std::set<int> const& squares) {
+    int largest = 0;
+    for(auto n : squares) {
+        std::string tmp = std::to_string(n);
+        // check if lengthes differ
+        if (x.size() != tmp.size())
+            continue;
+        std::map<char, int> dig;
+        for(int i = x.size() - 1; i >= 0; i--){
+            dig[x[i]] = n % 10; // map characters to integers
+            n /= 10;
+        }
+        if (repeat_mapping(dig))
+            continue;
+        // no leading zeros allowed dude
+        if (dig[y[0]] == 0)
+            continue;
+        int check = 0;
+        for(int i = 0; i < y.size(); ++i)
+            check = (10 * check) + (dig[y[i]]);
+        if(check > largest && squares.find(check) != squares.end())
+            largest = check;
+    }
+    return largest;
 }
